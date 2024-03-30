d25eb854e1ba972bc624ce16e62c6e52ef593496
DaoAnhThien <22521382@gm.uit.edu.com>
2024-03-30 15:16:47 +0700

    tcpchat

---
+++ TCPChat/TCPServer/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 71);
+            label1.TabIndex = 0;
+            label1.Text = "Server";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(95, 33);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(604, 34);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "127.0.0.1:9000";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(605, 413);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 84);
+            button1.TabIndex = 2;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(95, 83);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ScrollBars = ScrollBars.Both;
+            textBox2.Size = new Size(604, 284);
+            textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(442, 413);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 84);
+            button2.TabIndex = 4;
+            button2.Text = "Send";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(95, 373);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(604, 34);
+            textBox3.TabIndex = 5;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 350);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 71);
+            label2.TabIndex = 6;
+            label2.Text = "Message";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(705, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 71);
+            label3.TabIndex = 7;
+            label3.Text = "Client IP";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(705, 62);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(353, 384);
+            listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1070, 507);
+            Controls.Add(listBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "TCP Server";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+        private Button button2;
+        private TextBox textBox3;
+        private Label label2;
+        private Label label3;
+        private ListBox listBox1;
+    }
+}
---
+++ TCPChat/TCPServer/Form1.cs
@@ -0,0 +1,84 @@
+﻿using SuperSimpleTcp;
+using System.Text;
+
+namespace TCPServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpServer server;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            server = new SimpleTcpServer(textBox1.Text);
+            server.Events.ClientConnected += Events_ClientConnected;
+            server.Events.ClientDisconnected += Events_ClientDisconnected;
+            server.Events.DataReceived += Events_DataReceived;
+
+        }
+
+        private void Events_DataReceived(object? sender, DataReceivedEventArgs e)
+        {
+        
+            this.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += $"{e.IpPort} : {Encoding.UTF8.GetString(e.Data)}{Environment.NewLine}";
+            });
+        }
+
+        private void Events_ClientDisconnected(object? sender, ConnectionEventArgs e)
+        {
+           
+            this.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += $"{e.IpPort} đã ngắt.{Environment.NewLine}";
+                listBox1.Items.Remove(e.IpPort);
+            });
+        }
+
+        private void Events_ClientConnected(object? sender, ConnectionEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += $"{e.IpPort} đã kết nối.{Environment.NewLine}";
+                listBox1.Items.Add(e.IpPort);
+            });
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            server.Start();
+            textBox2.Text += $"Kết nối . . .{Environment.NewLine}";
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (server.IsListening)
+            {
+                if (!string.IsNullOrEmpty(textBox3.Text) && listBox1.SelectedItem != null )
+                {
+                    server.Send (listBox1.SelectedItem.ToString(), textBox3.Text);
+                    textBox2.Text += $"Server: {textBox3.Text}{Environment.NewLine}";
+                    textBox3.Text = string.Empty ;
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
